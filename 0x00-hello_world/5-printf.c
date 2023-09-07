d8aa6cde4941369292e3f0ce86d2d4fabf612c3c
Rsilimwe <silimwerodgers07@gmail.com>
2023-09-07 10:01:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
 * main - A program that prints a line with puts function
@@ -6,6 +5,5 @@
 */ 
 int main(void) 
 {
-    puts("\ ”Programming is like building a multilingual puzzle");
-    return 0;
-}
+puts("\ ”Programming is like building a multilingual puzzle");
+return 0;}
