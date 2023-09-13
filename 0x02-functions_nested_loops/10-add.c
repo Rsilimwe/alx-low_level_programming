c9326cdc260afc4dd95d06c0fac5214e89839bda
Rsilimwe <silimwerodgers07@gmail.com>
2023-09-13 14:25:03 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,27 +2,12 @@
 
 /**
  * add - Adds two integers.
- * @a: The first integer
- * @b: The second integer
+ * @a: First integer.
+ * @b: Second integer.
  *
- * Return: The sum of a and b
+ * Return: The sum of the two integers.
  */
 int add(int a, int b)
 {
 return (a + b);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-int num1 = 10;
-int num2 = 20;
-int sum;
-sum = add(num1, num2);
-printf("The sum of %d and %d is: %d\n", num1, num2, sum);
-return (0);
 }
